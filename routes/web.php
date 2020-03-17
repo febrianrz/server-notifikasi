230020e84b65fcb5a81daa2d1ff762e29584b9c9
febrian reza <febrianrz@gmail.com>
2020-03-17 23:08:55 +0700

    Add redirect to alter on main page

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,5 @@
 <?php
 
-use App\Notification;
-use App\Mail\NotifMaillable;
-use Illuminate\Support\Facades\Mail;
-use Febrianrz\Micronotification\Notif;
-
-Route::get('/tesnotif', function(Request $request){
-    Notif::send('','');
+Route::get('/', function(){
+    return redirect('https://alterindonesia.com');
 });
