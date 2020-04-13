bebcaf0b881adbbc7813b7660d8a41ca6bb3d723
febrian reza <febrianrz@gmail.com>
2020-04-13 09:22:41 +0700

    fixing bugs spesific key too length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Notification;
 use App\Observers\NotificationObserve;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         Notification::observe(NotificationObserve::class);
     }
 }
