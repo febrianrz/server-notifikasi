bca9d8be203127f77848c682c31e4fbd96caba02
febrian reza <febrianrz@gmail.com>
2020-04-13 09:25:59 +0700

    init seeder

--- config/micro.php
+++ config/micro.php
@@ -4,12 +4,12 @@ return [
     'app'   => [
         'id'        => 2,
         'secret'    => 'xxx',
-        'redirect_uri'=> ''
+        'redirect_uri'=> '',
+        'auth_path'     => ''
     ],
     'url'   => [
         'auth'          => 'https://accounts.rumahkepemimpinan.org',
         'notification'  => 'http://localhost:8002',
-        'auth_path'     => ''
     ],
     'endpoint'=> [
         'profile'   => '/api/v1/profile',
